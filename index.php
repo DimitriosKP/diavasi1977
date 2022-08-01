b42b059e3742ec4b5a53b6b1589e04b6f3d1d1a6
DimitriosKP <dimitrios.peftitsis@gmail.com>
2022-08-01 16:03:00 +0300

    rating add

--- index.php
+++ index.php
@@ -66,7 +66,18 @@
         <div class="info">
             <section class="cards-area" id="info-section">
                 <h1>Ποιοί είμαστε</h1>
-                <p class="text-justify">Μια από τις καθιερωµένες αξίες για καλό φαγητό στην πόλη, το εστιατόριο Διάβαση συνεχίζει από το 1977 να δηµιουργεί... πιστούς θαµώνες. Ξεκινώντας από τον Άγγελο Χαντακή, έναν από τους καλύτερους ψήστες στη Θεσσαλονίκη, master του γύρου, σήµερα τα ηνία έχει αναλάβει η... επόµενη γενιά, στην οποία ο ιδρυτής έχει µεταλαµπαδεύσει την εµπειρία τόσων χρόνων αλλά και το µεράκι του. Συνδυάζοντας παραδοσιακά και µοντέρνα στοιχεία στην αισθητική του, ξεχωρίζει εδώ και δεκαετίες για τα ζουµερά και πεντανόστιµα σουτζουκάκια του και φυσικά τον... διάσηµο γύρο του, που συνοδεύονται άψογα από σπιτικές πατάτες, χειροποίητη ρώσικη αλλά και πληθωρικές δροσερές σαλάτες, όλα από αγνές πρώτες ύλες. Δεν υπάρχει επώνυµος και µη Θεσσαλονικιός που να µην έχει περάσει από εδώ και να µην έχει φύγει µε τις καλύτερες εντυπώσεις για την ποιότητα, τις χορταστικές µερίδες και τις εξαιρετικές τιµές. Στα συν η ενηµερωµένη κάβα ελληνικών κυρίως κρασιών. Όσο για το service,... το υψηλό του επίπεδο θα το διαπιστώσετε µόνοι σας! Χώρος όμορφα διακοσμημένος, κατάλληλος για να νοιώσετε οικειότητα από την πρώτη στιγμή. Σπεσιαλιτέ της Διάβασης είναι τα σουτζουκάκια και ο γύρος, ενώ διαθέτει μεγάλη ποικιλία σε ψητά της ώρας. Συνοδέψτε τα με φρεσκοκομμένες τηγανιτές πατάτες, μελιτζανοσαλάτα ή ρώσικη, καθώς και με το αγαπημένο σας κρασί ή ποτό, αφού θα το βρείτε σίγουρα μέσα από την μεγάλη κάβα που διαθέτει.</p>
+                <p class="text-justify">Μια από τις καθιερωµένες αξίες για καλό φαγητό στην πόλη, το εστιατόριο Διάβαση συνεχίζει από το 1977 
+                    να δηµιουργεί... πιστούς θαµώνες. Ξεκινώντας από τον Άγγελο Χαντακή, έναν από τους καλύτερους ψήστες στη Θεσσαλονίκη, 
+                    master του γύρου, σήµερα τα ηνία έχει αναλάβει η... επόµενη γενιά, στην οποία ο ιδρυτής έχει µεταλαµπαδεύσει την εµπειρία 
+                    τόσων χρόνων αλλά και το µεράκι του. Συνδυάζοντας παραδοσιακά και µοντέρνα στοιχεία στην αισθητική του, ξεχωρίζει εδώ και 
+                    δεκαετίες για τα ζουµερά και πεντανόστιµα σουτζουκάκια του και φυσικά τον... διάσηµο γύρο του, που συνοδεύονται άψογα από 
+                    σπιτικές πατάτες, χειροποίητη ρώσικη αλλά και πληθωρικές δροσερές σαλάτες, όλα από αγνές πρώτες ύλες. Δεν υπάρχει επώνυµος 
+                    και µη Θεσσαλονικιός που να µην έχει περάσει από εδώ και να µην έχει φύγει µε τις καλύτερες εντυπώσεις για την ποιότητα, 
+                    τις χορταστικές µερίδες και τις εξαιρετικές τιµές. Στα συν η ενηµερωµένη κάβα ελληνικών κυρίως κρασιών. Ο όμορφα 
+                    διακοσμημένος χώρος, η υψηλού επιπέδου εξυπηρέτηση σε συνδυασμό με τις υψηλού επιπέδου γεύσεις θα σας οδηγήσουν σε μια 
+                    οικειότητα από την πρώτη κιόλας στιγμή. Σπεσιαλιτέ της Διάβασης είναι τα σουτζουκάκια και ο γύρος, ενώ διαθέτει μεγάλη 
+                    ποικιλία σε ψητά της ώρας. Συνοδέψτε τα με φρεσκοκομμένες τηγανιτές πατάτες, μελιτζανοσαλάτα ή ρώσικη, καθώς και με το 
+                    αγαπημένο σας κρασί ή ποτό, αφού θα το βρείτε σίγουρα μέσα από την μεγάλη κάβα που διαθέτει.</p>
                 <div class="row">
                     <div class="col-sm-4">
                         <div class="thumbnail">
@@ -102,89 +113,6 @@
             </div>
         </section>
         <br>
-        <!-------RATING-------->
-        <h1>Αξιολογήστε μας</h1>
-        <div align="center" style="background-color: rgb(17, 17, 17); padding: 50px; color:white;">
-            <i class="fa fa-star fa-2x" data-index="0"></i>
-            <i class="fa fa-star fa-2x" data-index="1"></i>
-            <i class="fa fa-star fa-2x" data-index="2"></i>
-            <i class="fa fa-star fa-2x" data-index="3"></i>
-            <i class="fa fa-star fa-2x" data-index="4"></i>
-            <br><br>
-            <p>Βαθμολογία</p><?php echo round($avg,2) ?>
-        </div>
-
-        <script src="http://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
-        <script>   
-            var ratedIndex = -1, uID = 0;
-
-            $(document).ready(function () {
-                resetStarColors();
-
-                if (localStorage.getItem('ratedIndex') != null) {
-                    setStars(parseInt(localStorage.getItem('ratedIndex')));
-                    uID = localStorage.getItem('uID');
-                }
-
-                $('.fa-star').on('click', function () {
-                ratedIndex = parseInt($(this).data('index'));
-                localStorage.setItem('ratedIndex', ratedIndex);
-                saveToTheDB();
-                });
-
-                $('.fa-star').mouseover(function () {
-                    resetStarColors();
-                    var currentIndex = parseInt($(this).data('index'));
-                    setStars(currentIndex);
-                });
-
-                $('.fa-star').mouseleave(function () {
-                    resetStarColors();
-
-                    if (ratedIndex != -1)
-                        setStars(ratedIndex);
-                });
-            });
-
-            function saveToTheDB() {
-                $.ajax({
-                url: "index.php",
-                method: "POST",
-                dataType: 'json',
-                data: {
-                    save: 1,
-                    uID: uID,
-                    ratedIndex: ratedIndex
-                }, success: function (r) {
-                        uID = r.id;
-                        localStorage.setItem('uID', uID);
-                }
-                });
-            }
-
-            function setStars(max) {
-                for (var i=0; i <= max; i++)
-                    $('.fa-star:eq('+i+')').css('color', 'gold');
-            }
-
-            function resetStarColors() {
-                $('.fa-star').css('color', 'white');
-            }
-
-            const btn = document.querySelector("button");
-            const post = document.querySelector(".post");
-            const widget = document.querySelector(".star-widget");
-            const editBtn = document.querySelector(".edit");
-            btn.onclick = ()=>{
-            widget.style.display = "none";
-            post.style.display = "block";
-            editBtn.onclick = ()=>{
-                widget.style.display = "block";
-                post.style.display = "none";
-            }
-            return false;
-        }
-        </script>
         
         <?php require_once 'footer.php'; ?>
 
---
+++ rating.php
@@ -0,0 +1,83 @@
+ <!-------RATING-------->
+ <h1>Αξιολογήστε μας</h1>
+        <div align="center" style="background-color: rgb(17, 17, 17); padding: 50px; color:white;">
+            <i class="fa fa-star fa-2x" data-index="0"></i>
+            <i class="fa fa-star fa-2x" data-index="1"></i>
+            <i class="fa fa-star fa-2x" data-index="2"></i>
+            <i class="fa fa-star fa-2x" data-index="3"></i>
+            <i class="fa fa-star fa-2x" data-index="4"></i>
+            <br><br>
+            <p>Βαθμολογία</p><?php echo round($avg,2) ?>
+        </div>
+
+        <script src="http://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
+        <script>   
+            var ratedIndex = -1, uID = 0;
+
+            $(document).ready(function () {
+                resetStarColors();
+
+                if (localStorage.getItem('ratedIndex') != null) {
+                    setStars(parseInt(localStorage.getItem('ratedIndex')));
+                    uID = localStorage.getItem('uID');
+                }
+
+                $('.fa-star').on('click', function () {
+                ratedIndex = parseInt($(this).data('index'));
+                localStorage.setItem('ratedIndex', ratedIndex);
+                saveToTheDB();
+                });
+
+                $('.fa-star').mouseover(function () {
+                    resetStarColors();
+                    var currentIndex = parseInt($(this).data('index'));
+                    setStars(currentIndex);
+                });
+
+                $('.fa-star').mouseleave(function () {
+                    resetStarColors();
+
+                    if (ratedIndex != -1)
+                        setStars(ratedIndex);
+                });
+            });
+
+            function saveToTheDB() {
+                $.ajax({
+                url: "index.php",
+                method: "POST",
+                dataType: 'json',
+                data: {
+                    save: 1,
+                    uID: uID,
+                    ratedIndex: ratedIndex
+                }, success: function (r) {
+                        uID = r.id;
+                        localStorage.setItem('uID', uID);
+                }
+                });
+            }
+
+            function setStars(max) {
+                for (var i=0; i <= max; i++)
+                    $('.fa-star:eq('+i+')').css('color', 'gold');
+            }
+
+            function resetStarColors() {
+                $('.fa-star').css('color', 'white');
+            }
+
+            const btn = document.querySelector("button");
+            const post = document.querySelector(".post");
+            const widget = document.querySelector(".star-widget");
+            const editBtn = document.querySelector(".edit");
+            btn.onclick = ()=>{
+            widget.style.display = "none";
+            post.style.display = "block";
+            editBtn.onclick = ()=>{
+                widget.style.display = "block";
+                post.style.display = "none";
+            }
+            return false;
+        }
+        </script>
